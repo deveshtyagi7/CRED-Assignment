acc2dcb01582da390c374f5f4d832ae954efe9e7
Devesh Tyagi <devesh.tyagi@etmoney.com>
2023-11-25 13:24:40 +0530

    Initial Commit

---
+++ Cred Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cred Task
+//
+//  Created by Devesh Tyagi on 25/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
